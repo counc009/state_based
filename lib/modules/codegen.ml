0073db24b401ff1db90ccaab1361368d906820de
Aaron Councilman <aaronjc4@illinois.edu>
2024-12-02 12:02:08 -0600

    Starting to process module definitions

--- lib/modules/codegen.ml
+++ lib/modules/codegen.ml
@@ -27,14 +27,19 @@ type typ = Bool | Int | Float | String | Path | Unit
          | Enum        of (typ list) StringMap.t
          | Placeholder of typ placeholder
 
+let rec type_equality (x : typ) (y : typ) : bool =
+  match x, y with
+  | Placeholder { contents = Some x }, _ -> type_equality x y
+  | _, Placeholder { contents = Some y } -> type_equality x y
+  | _, _ -> x = y
+
 type module_info =
   { name : string list;
     (* Alias map maps from aliases to their canonical name *)
     alias_map : string StringMap.t;
     argument_types : typ StringMap.t;
-    required_arguments : StringSet.t;
     input_struct_def : typ StringMap.t;
-    out_type : typ option;
+    out_type : typ;
     body : Target.stmt placeholder }
 
 type type_env = typ UniqueMap.t
@@ -87,6 +92,65 @@ let rec process_type (t : Ast.typ) env : typ =
       match UniqueMap.find nm env with
       | Some t -> t
       | None -> failwith (Printf.sprintf "undefined type %s" nm)
+
+let process_type_option (t : Ast.typ option) env : typ =
+  match t with
+  | None -> Unit
+  | Some t -> process_type t env
+
+let process_module_for_args (body : Ast.stmt list) env
+  : string StringMap.t * typ StringMap.t * typ StringMap.t = 
+  let rec add_alias alias nm aliases =
+    match alias with
+    | [] -> aliases
+    | v :: tl ->
+        match StringMap.find_opt v aliases with
+        | None -> add_alias tl nm (StringMap.add v nm aliases)
+        | Some n ->
+            if n = nm then add_alias tl nm aliases
+            else failwith "variable already used as alias with different canonical name"
+  in let rec add_vars vars aliases var_types struct_def =
+    match vars with
+    | [] -> (aliases, var_types, struct_def)
+    (*| (nm, alias, typ, _) :: tl -> ???*)
+    | (nm, alias, typ, _) :: tl ->
+        let typ = process_type typ env
+        in let var_types =
+          match StringMap.find_opt nm aliases with
+          | Some _ -> failwith "variable already used as alias"
+          | None ->
+              match StringMap.find_opt nm var_types with
+              | None -> StringMap.add nm typ var_types
+              | Some t ->
+                  if type_equality t typ then var_types
+                  else failwith "variable already used with incompatible types"
+        in let aliases = add_alias alias nm aliases
+        in add_vars tl aliases var_types struct_def
+  in let rec process (body : Ast.stmt list) aliases var_types struct_def =
+    match body with
+    | [] -> (aliases, var_types, struct_def)
+    | RequiredVar vars :: tl | OptionalVar vars :: tl ->
+        let (aliases, var_types, struct_def)
+          = add_vars vars aliases var_types struct_def
+        in process tl aliases var_types struct_def
+    | ForLoop (_, _, body) :: tl ->
+        let (aliases, var_types, struct_def)
+          = process body aliases var_types struct_def
+        in process tl aliases var_types struct_def
+    | IfProvided (_, thn, els) :: tl | IfThenElse (_, thn, els) :: tl ->
+        let (aliases, var_types, struct_def)
+          = process thn aliases var_types struct_def
+        in let (aliases, var_types, struct_def)
+          = process els aliases var_types struct_def
+        in process tl aliases var_types struct_def
+    | Match (_, cases) :: tl ->
+        List.fold_left
+          (fun (aliases, var_types, struct_def) (_, case)
+            -> process case aliases var_types struct_def)
+          (process tl aliases var_types struct_def)
+          cases
+    | _ :: tl -> process tl aliases var_types struct_def
+  in process body StringMap.empty StringMap.empty StringMap.empty
 
 (* process_stmt is used to handle statements in functions/Ansible (and loop
  * bodies in modules) while process_module is used to handle statements in
@@ -159,8 +223,16 @@ let codegen (files : Ast.topLevel list list) : type_env * global_env =
   in let rec create_functions (ts : Ast.topLevel list) types env =
     match ts with
     | [] -> ()
-    | Function (_, _, _, _) :: tl -> create_functions tl types env
-    | Module (_, _, _) :: tl -> create_functions tl types env
+    | Function (nm, args, ret, _) :: tl ->
+        let arg_tys = List.map (fun (_, t) -> process_type t types) args
+        and ret_ty  = process_type_option ret types
+        in UniqueMap.add nm (Function (nm, arg_tys, ret_ty, ref None)) env
+        ; create_functions tl types env
+    | Module (_, ret, body) :: tl ->
+        let (_, _, _)
+          = process_module_for_args body types
+        and _ = process_type_option ret types
+        in create_functions tl types env
     | _ :: _ -> failwith "partitioning error"
 
   in let (tys, dfs, fns) = partition files
