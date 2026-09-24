2553df27256e9715c268f35bfd898839447b80d2
Aaron Councilman <aaronjc4@illinois.edu>
2026-09-24 15:35:35 -0500

    Add runner code

--- bash-verifier/bin/main.ml
+++ bash-verifier/bin/main.ml
@@ -1,18 +1,29 @@
 open Frontend
 
-let errored = ref false
-
-let process_file filename =
+let parse_file filename =
   try
     let res = Runner.parse_file filename
-    in Printf.printf "File %s\n\n%s\n\n" filename (Format.string_of_ast res)
+    in Ok res
   with
   | Lexer.LexerError msg ->
-    Printf.printf "While processing file %s encountered:\n  %s\n" filename msg;
-    errored := true
-  | Sys_error _ ->
-    Printf.printf "Failed to read file %s\n" filename;
-    errored := true
+      Error (Printf.sprintf "While processing file %s, encountered:\n  %s\n"
+              filename msg)
+  | Sys_error _->
+      Error (Printf.sprintf "Failed to read file %s\n" filename)
+
+let process_files files =
+  let (decls, errors) =
+    List.fold_left (fun (decls, errors) file ->
+      match parse_file file with
+      | Ok res -> (res @ decls, errors)
+      | Error msg -> print_string msg; (decls, true)
+    ) ([], false) files
+  in if errors
+  then false
+  else
+    match Semant.analyze_program decls with
+    | Ok _env -> Printf.printf "SUCCESS!\n"; true
+    | Err (_env, errs) -> Runner.print_semant_errors errs; false
 
 let () =
   let () = Clap.description "State Calculus Front-End Compiler"
@@ -20,7 +31,6 @@ let () =
     Clap.list_string ~placeholder:"FILENAME"
       ~description:"Files to process (order does not matter)" ()
   in let () = Clap.close ()
-  in let () = List.iter process_file files
-  in if !errored
-  then exit 1
-  else ()
+  in if process_files files
+  then ()
+  else exit 1
--- bash-verifier/lib/frontend/runner.ml
+++ bash-verifier/lib/frontend/runner.ml
@@ -6,4 +6,16 @@ let parse_channel c =
   let lexbuf = Lexing.from_channel c
   in Parser.program Lexer.token lexbuf
 
-let parse_file f = parse_channel (open_in f)
+let parse_file f =
+  let lexbuf = Lexing.from_channel (open_in f)
+  in Lexing.set_filename lexbuf f; Parser.program Lexer.token lexbuf
+
+let rec print_semant_errors (errs : Semant.err_msg) : unit =
+  match errs with
+  | Leaf { pos = (x, y); msg } ->
+      Printf.printf "%s (%d,%d -- %d,%d) error : %s\n"
+        x.pos_fname
+        x.pos_lnum (x.pos_cnum - x.pos_bol + 1)
+        y.pos_lnum (y.pos_cnum - y.pos_bol)
+        msg
+  | Node (x, y) -> print_semant_errors x; print_semant_errors y
